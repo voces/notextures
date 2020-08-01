56536747e8a30cbd313c40ecbdb3d80bfefefa9e
Robert Coe <chakrawc@gmail.com>
2020-07-31 19:20:37 -0700

    improve viewer... slightly

--- viewer/scene.js
+++ viewer/scene.js
@@ -21,7 +21,7 @@ const camera = new PerspectiveCamera(
 	0.1,
 	1000,
 );
-camera.position.z = 2;
+camera.position.z = 20;
 camera.position.y = -1.5;
 camera.rotation.x = 0.7;
 
@@ -59,12 +59,30 @@ const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Camera movements
+const keyboard = {};
+let pause = false;
+window.addEventListener("keydown", (e) => (keyboard[e.key] = true));
+window.addEventListener("keyup", (e) => (keyboard[e.key] = false));
+window.addEventListener("mousewheel", (e) => {
+	if (e.deltaY > 0) camera.position.z *= 1.1;
+	else camera.position.z /= 1.1;
+});
+window.addEventListener("mousedown", () => (pause = true));
+window.addEventListener("mouseup", () => (pause = false));
+
 function render() {
 	requestAnimationFrame(render);
 
-	for (let i = 0; i < scene.children.length; i++)
-		if (typeof scene.children[i].update === "function")
-			scene.children[i].update();
+	if (!pause)
+		for (let i = 0; i < scene.children.length; i++)
+			if (typeof scene.children[i].update === "function")
+				scene.children[i].update();
+
+	if (keyboard.ArrowLeft) camera.position.x -= 0.02 * camera.position.z;
+	if (keyboard.ArrowRight) camera.position.x += 0.02 * camera.position.z;
+	if (keyboard.ArrowUp) camera.position.y += 0.02 * camera.position.z;
+	if (keyboard.ArrowDown) camera.position.y -= 0.02 * camera.position.z;
 
 	renderer.render(scene, camera);
 }
