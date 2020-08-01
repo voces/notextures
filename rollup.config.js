760b46ccc4424c891fb001e3aa2f58815a2d751d
Robert Coe <chakrawc@gmail.com>
2020-08-01 09:38:44 -0700

    public -> docs

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "rollup-plugin-typescript2";
 export default [
 	{
 		input: "viewer/main.ts",
-		output: [{ file: "public/bundle.js", format: "es" }],
+		output: [{ file: "docs/bundle.js", format: "es" }],
 		plugins: [
 			typescript({
 				tsconfigOverride: { compilerOptions: { declaration: false } },
